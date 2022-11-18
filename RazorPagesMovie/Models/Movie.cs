0f50fd46f2e1d71a5ec7fadca2fe7a667d3afadd
veyselboybay <veysel.boybay@outlook.com>
2022-11-18 17:24:57 -0500

    Data Model created

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
